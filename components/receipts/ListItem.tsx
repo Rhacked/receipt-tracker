a81f574479455e7520fbd4ea6b5d014abc63442d
Ruben Hagen <ruben.hagen@vg.no>
2025-08-26 19:52:32 +0200

    More receipt styling

--- components/receipts/ListItem.tsx
+++ components/receipts/ListItem.tsx
@@ -10,14 +10,22 @@ export default function ListItem({ receipt }: { receipt: ReceiptType }) {
         className="hover:cursor-pointer"
         onClick={() => setExpanded(!expanded)}
       >
-        {receipt.store} - {receipt.total} {receipt.currency} -&nbsp;
+        <strong>{receipt.store}</strong> -{" "}
+        {receipt.total.toLocaleString("no-NB", {
+          minimumFractionDigits: 2,
+        })}{" "}
+        {receipt.currency} -&nbsp;
         {new Date(receipt.datetime).toLocaleDateString("no-NB")}
       </div>
       {expanded && (
         <div className="pl-4 space-y-1 mt-1">
           {receipt.line_items.map((item, index) => (
             <div key={index}>
-              {item.description} - {item.category} - {item.amount} -{" "}
+              {item.description} - {item.category} -{" "}
+              {item.amount.toLocaleString("no-NB", {
+                minimumFractionDigits: 2,
+              })}
+              &nbsp;
               {receipt.currency}
             </div>
           ))}
